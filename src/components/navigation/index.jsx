153c15dbd3f70b6087fa18efb156ee8327e72fa2
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-04-03 14:54:55 +0530

    starting new design working on navigation bar

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { getRequest } from "../../functions/get.req"
 import { Link } from "react-router-dom"
 import { imageList } from "../../functions/images"
-import { AiOutlineShop, AiOutlineUser, AiTwotoneShop } from 'react-icons/ai'
+import { AiOutlineShop, AiOutlineShoppingCart, AiOutlineUser, AiTwotoneShop } from 'react-icons/ai'
 
 export default function Navigation({ls, ud, um}) {
 
@@ -20,31 +20,28 @@ export default function Navigation({ls, ud, um}) {
 
     return (
         <div className="nav" style={{backgroundImage: "url(" + imageList[2] + ")"}}>
-            <div className="nav-left">
-                <img src={imageList[0]} alt="" style={{filter: "invert(1)"}} />
+            <div className="top">
+                <Link className="top-link">SHIP TO: India</Link>
+                <Link className="top-link">GIFT CARDS</Link>
+                <Link className="top-link">STORE LOCATION</Link>
+                <Link to="user/profile" className="top-link">MY ACCOUNT</Link>
             </div>
-            <div className="nav-links-ar">
-                <div className="nav-link">
-                    {
-                        navigation.map((link, i)=>{
-                            return <Link className={clicked===link.path ? "link button-in color-white hover active" : "link button-in color-white hover inactive"} to={link.path} key={i} onClick={()=>setClicked(link.path)}>{link.name}</Link>
-                        })
-                    }
+            <div className="bottom">
+                <div className="nav-left">
+                    <img src={imageList[0]} alt="" style={{filter: "invert(1)"}} />
                 </div>
-                <div className="nav-controls">
-                    <Link to="/user/cart" className="nav-icon">
-                        <AiOutlineShop />
-                    </Link>
-                    <div className="profile-ar">
-                        <Link to="/user/profile" className="profile-icon" >
-                            <img src={um.profile_photo && um.profile_photo.small} className="profile-image" />
-                            <AiOutlineUser className="profile-info" onClick={()=>setClicked("null")}/>
+                <div className="nav-links-ar">
+                    <div className="nav-link">
+                        {
+                            navigation.map((link, i)=>{
+                                return <Link className={clicked===link.path ? "link button-in color-white hover active" : "link button-in color-white hover inactive"} to={link.path} key={i} onClick={()=>setClicked(link.path)}>{link.name}</Link>
+                            })
+                        }
+                    </div>
+                    <div className="nav-controls">
+                        <Link to="/user/cart" className="nav-icon">
+                            <AiOutlineShoppingCart />
                         </Link>
-                        <div className="dropdown">
-                            {
-                                ud.email
-                            }
-                        </div>
                     </div>
                 </div>
             </div>
