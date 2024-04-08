2b1d3533148e4dd904283d6e77db8c35cb313a3f
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-04-09 03:14:11 +0530

    changes to global

--- src/functions/url.js
+++ src/functions/url.js
@@ -2,6 +2,6 @@ const url_local = "http://localhost:1000/";
 const url_global = "http://3.91.203.73:1000/";
 const url_public = "http://192.168.65.159:1000/"
 
-const url_main = url_public;
+const url_main = url_global;
 
 export default url_main;
