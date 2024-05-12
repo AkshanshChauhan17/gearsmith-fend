89612f1d9f586dfd7376e204a935bff0bf159cc5
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-05-12 14:14:02 +0530

    sd

--- src/functions/url.js
+++ src/functions/url.js
@@ -1,5 +1,5 @@
 const url_local = "http://localhost:1000/";
-const url_global = "http://gearsmith.shop/api/";
+const url_global = "https://gearsmith.shop/api/";
 const url_public = "http://192.168.179.159:1000/"
 
 const url_main = url_global;
